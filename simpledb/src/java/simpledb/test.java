8df22cc2938d198607378b05fcd9e6a3619839b0
marS24-0 <marS24-0>
2022-11-18 10:50:02 +0100

    final test

---
+++ simpledb/src/java/simpledb/test.java
@@ -0,0 +1,32 @@
+package simpledb;
+
+import java.io.*;
+
+public class test {
+    public static void main(String[] argv) {
+        // construct table schema
+        Type types[] = new Type[] { Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE };
+        String names[] = new String[] { "f0", "f1", "f2", "f3" };
+        TupleDesc descriptor = new TupleDesc(types, names);
+        // create the table, associate it with some_data_file.dat
+        // and tell the catalog about the schema of this table.
+        HeapFile table1 = new HeapFile(new File("some_data_file.dat"), descriptor);
+        Database.getCatalog().addTable(table1, "test");
+        // construct the query: we use a simple SeqScan, which spoonfeeds
+        // tuples via its iterator.
+        TransactionId tid = new TransactionId();
+        SeqScan f = new SeqScan(tid, table1.getId());
+        try {
+            // and run it
+            f.open();
+            while (f.hasNext()) {
+                Tuple tup = f.next();
+                System.out.println(tup);
+            }
+            f.close();
+            Database.getBufferPool().transactionComplete(tid);
+        } catch (Exception e) {
+            System.out.println("Exception : " + e);
+        }
+    }
+}
